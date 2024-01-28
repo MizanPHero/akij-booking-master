9a29d2f3b137e1dbc4c619c411bf60059779577c
Mizanur Rahman <mizanrxb@gmail.com>
2024-01-28 06:40:35 +0600

    search icon added

--- src/components/From.jsx
+++ src/components/From.jsx
@@ -32,15 +32,15 @@ const From = ({handleFromSelect, fromSelectedOption, airports}) => {
           <p className="font-normal text-sm text-[#4A4A4A]">From</p>
           <p className="text-[26px] font-black text-black">
             {fromSelectedOption?.name
-              ? fromSelectedOption.name.length > 12
-                ? `${fromSelectedOption.name.slice(0, 12)}..`
+              ? fromSelectedOption.name.length > 13
+                ? `${fromSelectedOption.name.slice(0, 13)}..`
                 : fromSelectedOption.name
               : "Select"}
           </p>
           <p className="font-normal text-sm text-[#4A4A4A]">
             {fromSelectedOption?.airport
-              ? fromSelectedOption.airport.length > 22
-                ? `${fromSelectedOption.airport.slice(1, 22)}...`
+              ? fromSelectedOption.airport.length > 29
+                ? `${fromSelectedOption.airport.slice(1, 29)}...`
                 : fromSelectedOption.airport
               : "Select your destination"}
           </p>
@@ -54,13 +54,26 @@ const From = ({handleFromSelect, fromSelectedOption, airports}) => {
             aria-labelledby="menu-button"
             tabIndex="-1"
           >
-            <input
+            {/* <input
               className='block w-[99%] mx-auto h-8 px-3 py-2 my-1 text-sm bg-white border rounded-md shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
               type="text"
               placeholder='Search...'
               onClick={handleInputClick}
               onChange={(e) => setSearchTitle(e.target.value)}
-            />
+            /> */}
+
+
+              <label className="relative block w-[99%] mx-auto">
+                <span className="sr-only">Search</span>
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                  <svg className="w-5 h-5 fill-slate-300" viewBox="0 0 20 20">
+                    <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path>
+                  </svg>
+                </span>
+                <input onChange={(e) => setSearchTitle(e.target.value)} onClick={handleInputClick} className="block w-full py-2 pr-3 mt-1 bg-white border rounded-md shadow-sm placeholder:italic placeholder:text-slate-400 border-slate-300 pl-9 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search..." type="text"/>
+              </label>
+
+
             {airports
               .filter((value) => {
                 if (searchTitle === "") {
--- src/components/To.jsx
+++ src/components/To.jsx
@@ -29,15 +29,15 @@ const To = ({handleToSelect, toSelectedOption, airports}) => {
                 <p className="font-normal text-sm text-[#4A4A4A]">From</p>
                 <p className="text-[26px] font-black text-black">
                     {toSelectedOption?.name
-                        ? toSelectedOption.name.length > 12
-                            ? `${toSelectedOption.name.slice(0, 12)}..`
+                        ? toSelectedOption.name.length > 13
+                            ? `${toSelectedOption.name.slice(0, 13)}..`
                             : toSelectedOption.name
                         : "Select"}
                 </p>
                 <p className="font-normal text-sm text-[#4A4A4A]">
                     {toSelectedOption?.airport
-                        ? toSelectedOption.airport.length > 22
-                            ? `${toSelectedOption.airport.slice(1, 22)}...`
+                        ? toSelectedOption.airport.length > 29
+                            ? `${toSelectedOption.airport.slice(1, 29)}...`
                             : toSelectedOption.airport
                         : "Select your destination"}
                 </p>
@@ -54,13 +54,40 @@ const To = ({handleToSelect, toSelectedOption, airports}) => {
                     aria-labelledby="menu-button"
                     tabIndex="-1"
                 >
-                    <input
+            {/* <input
               className='block w-[99%] mx-auto h-8 px-3 py-2 my-1 text-sm bg-white border rounded-md shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
               type="text"
               placeholder='Search...'
               onClick={handleInputClick}
               onChange={(e) => setSearchTitle(e.target.value)}
-            />
+            /> */}
+
+
+
+
+            <label className="relative block w-[99%] mx-auto">
+              <span className="sr-only">Search</span>
+              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg className="w-5 h-5 fill-slate-300" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path>
+                </svg>
+              </span>
+              <input onChange={(e) => setSearchTitle(e.target.value)} onClick={handleInputClick} className="block w-full py-2 pr-3 mt-1 bg-white border rounded-md shadow-sm placeholder:italic placeholder:text-slate-400 border-slate-300 pl-9 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search..." type="text"/>
+            </label>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             {airports
               .filter((value) => {
                 if (searchTitle === "") {
